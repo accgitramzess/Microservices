5370353cbe1291d7123ad58bc2da39d2beb3edf4
accgitramzess <accgitramzess@gmail.com>
2020-01-03 15:41:44 +0200

    created final structure of gradle script added first-microservice skeleton

---
+++ first-microservice/src/main/java/com/first_microservice/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.first_microservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome from Spring Boot!";
+    }
+}
